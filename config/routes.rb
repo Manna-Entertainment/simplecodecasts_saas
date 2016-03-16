df26df9a1c207a1ac157884a71e05d33eab4f33c
Gideon R <gideon@mannagames.com>
2016-03-16 07:33:08 +0000

    Hopefully fixed the error...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :contacts
-  # get '/contacts/new' => 'pages#about'
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
