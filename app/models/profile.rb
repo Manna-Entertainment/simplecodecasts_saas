4c02681c2f494f2209dde4d999c77da4b1041c13
Gideon R <gideon@mannagames.com>
2016-04-01 23:54:36 +0000

    Added Favicons, Profile Forms, and Create Actions

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,16 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
+    
+# class Profile < ActiveRecord::Base
+#   validates :name, presence: true
+# end
+ 
+# Person.create(name: "John Doe").valid? # => true
+# Person.create(name: nil).valid? # => false
+    
+    
+# class Profile < ActiveRecord::Base
+#     validates :points, numericality: true
+#     validates :games_played, numericality: { only_integer: true }
+# end
 end
